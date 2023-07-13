28f488be71c2c1baad412c99751b797c01c77ad1
Okellothomas <thomasolal33@gmail.com>
2023-07-13 20:51:09 +0300

    add changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-  setTimeout(removeSplashScreen, 10000); // Remove splash screen after 3 seconds
+  setTimeout(removeSplashScreen, 14000); // Remove splash screen after 3 seconds
 });
 
 function removeSplashScreen() {
@@ -9,3 +9,19 @@ function removeSplashScreen() {
   // Add code to navigate to the next screen
   window.location.href = 'next.html';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var welcomeMessage = document.querySelector('.welcome-message');
+
+  function showMessage() {
+    welcomeMessage.style.display = 'block';
+    setTimeout(hideMessage, 2000);
+  }
+
+  function hideMessage() {
+    welcomeMessage.style.display = 'none';
+    setTimeout(showMessage, 2000);
+  }
+
+  showMessage();
+});
